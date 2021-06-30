90ddb1abebb653b451ddc0431339f272ff848a7b
opusvar <sprojdev@gmail.com>
2021-06-30 16:41:23 -0400

    Fixed file linking problems between scripts and between scripts and HTML, corrected syntax typos in coreScripts

--- scripts/coreScript.js
+++ scripts/coreScript.js
@@ -1,4 +1,5 @@
-import { createNode, append } from "./modules/domManipulator";
+import {append, createNode} from "./modules/domManipulator.js"
+
 console.log("Scripts connected")
 
 const ul = document.getElementById('authors')
@@ -8,10 +9,10 @@ window.addEventListener("click", () => {
     fetch(url)
     .then((response) => {
         response.json()
-        console.log(json)})
+        console.log(response)})
     .then((data) => {
         let authors = data.results;
-        return authors.map((author){
+        return authors.map((author) => {
             let li = createNode("li");
             let img = createNode("img");
             let span = createNode("span");
@@ -22,7 +23,7 @@ window.addEventListener("click", () => {
             append(ul, li);
         })
     })
-    .catch((error){
+    .catch((error) => {
         console.log(error)
     })
 })
